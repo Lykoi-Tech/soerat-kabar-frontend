6cebddd8ea6f42fed36c979f2b151e29208a8e8a
alyadulya <alyadulya@gmail.com>
2022-07-11 20:41:54 +0700

    add home page

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './index.scss';
 
 const Navigation = () => {
+  const [stickyNav, setStickyNav] = useState(false);
+
+  useEffect(() => {
+    window.onscroll = () => {
+      setStickyNav(window.pageYOffset === 0 ? false : true);
+      return () => (window.onscroll = null);
+    }
+  })
+
   return (
     <div>
-      <div className="navbar">
-        <h4 className="navbar-brand">Soerat Kabar</h4>
+      <div className={`navbar ${stickyNav ? 'sticky' : ''}`} id="navbar">
+        <p>Selasa, 5 Juli 2022, 10:34 WIB</p>
+        <div className="navbar-menu">
+          <a className="active" href="#beranda">Beranda</a>
+          <a href="#kategori">Kategori</a>
+          <a href="#arsip-nasional">Arsip Nasional</a>
+        </div>
       </div>
     </div>
   )
